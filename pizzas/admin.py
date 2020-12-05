895b6b0869b1af1b0fd47c569014c344196692b4
ajyeary <71294133+ajyeary@users.noreply.github.com>
2020-12-04 22:31:53 -0600

    Add files via upload

--- pizzas/admin.py
+++ pizzas/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import pizzas,topping
 
-admin.site.register(pizza)
+admin.site.register(pizzas)
 admin.site.register(topping)
